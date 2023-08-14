838ec98e1df58240bb36986e335a838c3e32503a
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-14 20:06:13 +0700

    update hw04-5

--- Homework/HW04/hw04-5-time-convert.cpp
+++ Homework/HW04/hw04-5-time-convert.cpp
@@ -13,3 +13,18 @@
     Output:
         27 days = 2332800 seconds
 */
+#include <stdio.h>
+
+int main() {
+    int days ;
+    long long seconds ;
+
+    printf( "Input Days :\n") ;
+    scanf( "%d" , &days) ;
+
+    seconds = (long long)days * 24 * 60 * 60 ; //Calculate and set the value of the seconds variable by multiplying the number of days entered by the user by 24 (hours per day) and 60 (minutes per hour) and 60 (seconds per minute).
+
+    printf( "%d days = %lld seconds\n" , days , seconds) ;
+
+    return 0;
+}//end fuction
